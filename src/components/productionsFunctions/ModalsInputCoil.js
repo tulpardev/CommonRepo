5f47012fcf37a2f99a1e8d01db12f8402b88d84b
tulpardev <suha.arikan@icloud.com>
2020-08-05 17:55:16 +0300

    ModalsInput updated

--- src/components/productionsFunctions/ModalsInputCoil.js
+++ src/components/productionsFunctions/ModalsInputCoil.js
@@ -21,10 +21,18 @@ function ModalsInputCoil() {
     setShow(false);
   };
 
+  function handleSave(event) {
+    event.preventDefault();
+    setShow(false);
+  }
   return (
     <>
       <Button variant="success" onClick={handleShow}>
         New
+      </Button>
+
+      <Button variant="primary" onClick={handleShow} style={{ marginLeft: 10 }}>
+        Edit
       </Button>
 
       <Modal
@@ -32,6 +40,8 @@ function ModalsInputCoil() {
         show={show}
         onHide={handleClose}
         size="xl"
+        backdrop="static"
+        keyboard={false}
       >
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
@@ -113,34 +123,60 @@ function ModalsInputCoil() {
             <Tab eventKey="generalData" title="Genaral Data">
               <Form>
                 <Container>
-                  {/* Stack the columns on mobile by making one full-width and the other half-width */}
                   <Row>
                     <Col>
                       <Form.Group
                         as={Row}
-                        controlId="formHorizontalScheduledId"
+                        controlId="formHorizontalSteelSteelGradeID"
                       >
                         <Form.Label column sm={4}>
-                          Schedule ID
+                        Steel Steel GradeID
                         </Form.Label>
                         <Col sm={8}>
-                          <Form.Control type="text" placeholder="Schedule ID" />
+                          <Form.Control as="select" type="text">
+                            <option>DC01</option>
+                            <option>DF01</option>
+                            <option>DD11</option>
+                            <option>DF600</option>
+                            <option>SF01</option>
+                            <option>DC01</option>
+                            <option>AF03</option>
+                            <option>DD11X</option>
+                            <option>S275JR</option>
+                            <option>SAE1010</option>
+                          </Form.Control>
                         </Col>
                       </Form.Group>
                     </Col>
                     <Col>
-                      <Form.Group
-                        as={Row}
-                        controlId="formHorizontalInputCoilId"
-                      >
+                      <Form.Group as={Row} controlId="formHorizontalWidth">
                         <Form.Label column sm={4}>
-                          Input Coil ID
+                          Width[mm]
                         </Form.Label>
                         <Col sm={8}>
-                          <Form.Control
-                            type="text"
-                            placeholder="Input Coil ID"
-                          />
+                          <Form.Control type="text" placeholder="Width" />
+                        </Col>
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Form.Group as={Row} controlId="formHorizontalWeight">
+                        <Form.Label column sm={4}>
+                          Weight[kg]
+                        </Form.Label>
+                        <Col sm={8}>
+                          <Form.Control type="text" placeholder="Weight" />
+                        </Col>
+                      </Form.Group>
+                    </Col>
+                    <Col>
+                      <Form.Group as={Row} controlId="formHorizontalLength">
+                        <Form.Label column sm={4}>
+                          Length[m]
+                        </Form.Label>
+                        <Col sm={8}>
+                          <Form.Control type="text" placeholder="Length" />
                         </Col>
                       </Form.Group>
                     </Col>
@@ -149,46 +185,31 @@ function ModalsInputCoil() {
                     <Col>
                       <Form.Group
                         as={Row}
-                        controlId="formHorizontalScheduleStatus"
+                        controlId="formHorizontaleHeadScrapLength"
                       >
                         <Form.Label column sm={4}>
-                          Schedule Status
+                          Head Scrap Length[m]
                         </Form.Label>
                         <Col sm={8}>
                           <Form.Control
                             type="text"
-                            placeholder="Schedule Status"
+                            placeholder="Head Scrap Length"
                           />
                         </Col>
                       </Form.Group>
                     </Col>
-                    <Col>
-                      <Form.Group as={Row} controlId="formHorizontalCoilStatus">
-                        <Form.Label column sm={4}>
-                          Coil Satatus
-                        </Form.Label>
-                        <Col sm={8}>
-                          <Form.Control
-                            type="email"
-                            placeholder="Coil Satatus"
-                          />
-                        </Col>
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Row>
                     <Col>
                       <Form.Group
                         as={Row}
-                        controlId="formHorizontalScheduleRemark"
+                        controlId="formHorizontalTailScrapLength"
                       >
-                        <Form.Label column sm={2}>
-                          Schedule Remark
+                        <Form.Label column sm={4}>
+                          Tail Scrap Length[m]
                         </Form.Label>
-                        <Col sm={10}>
+                        <Col sm={8}>
                           <Form.Control
                             type="text"
-                            placeholder="Schedule Remark"
+                            placeholder=" Tail Scrap Length"
                           />
                         </Col>
                       </Form.Group>
@@ -200,83 +221,77 @@ function ModalsInputCoil() {
             <Tab eventKey="processData" title="Process Data">
               <Form>
                 <Container>
-                  {/* Stack the columns on mobile by making one full-width and the other half-width */}
                   <Row>
                     <Col>
                       <Form.Group
                         as={Row}
-                        controlId="formHorizontalScheduledId"
+                        controlId="formHorizontalThermalCycle"
                       >
                         <Form.Label column sm={4}>
-                          Schedule ID
+                          Thermal Cycle
                         </Form.Label>
                         <Col sm={8}>
-                          <Form.Control type="text" placeholder="Schedule ID" />
+                          <Form.Control as="select" type="text">
+                            <option>CQ</option>
+                            <option>DQ</option>
+                            <option>DDQ</option>
+                            <option>EDDQ</option>
+                            <option>HSS1</option>
+                            <option>HSS2</option>
+                            <option>FH</option>
+                            <option>HR</option>
+                            <option>HSLA</option>
+                            <option>BH</option>
+                          </Form.Control>
                         </Col>
                       </Form.Group>
                     </Col>
                     <Col>
                       <Form.Group
                         as={Row}
-                        controlId="formHorizontalInputCoilId"
+                        controlId="formHorizontalExSteelGrade"
                       >
                         <Form.Label column sm={4}>
-                          Input Coil ID
+                          Ex. Steel Grade ID
                         </Form.Label>
                         <Col sm={8}>
-                          <Form.Control
-                            type="text"
-                            placeholder="Input Coil ID"
-                          />
+                          <Form.Control as="select" type="text">
+                            <option>DX51D+Z</option>
+                            <option>DX51D+Z100B</option>
+                            <option>DX52D+Z</option>
+                            <option>DX53D+Z</option>
+                            <option>DX54D+Z</option>
+                            <option>GR33</option>
+                            <option>GR50</option>
+                            <option>HCT590X</option>
+                            <option>S220GD+Z</option>
+                            <option>HX300LAD+Z</option>
+                          </Form.Control>
                         </Col>
                       </Form.Group>
                     </Col>
                   </Row>
                   <Row>
                     <Col>
-                      <Form.Group
-                        as={Row}
-                        controlId="formHorizontalScheduleStatus"
-                      >
+                      <Form.Group as={Row} controlId="formHorizontaSleeve">
                         <Form.Label column sm={4}>
-                          Schedule Status
+                          Sleeve
                         </Form.Label>
                         <Col sm={8}>
-                          <Form.Control
-                            type="text"
-                            placeholder="Schedule Status"
-                          />
+                          <Form.Control as="select" type="text">
+                            <option>YES</option>
+                            <option>NO</option>
+                          </Form.Control>
                         </Col>
                       </Form.Group>
                     </Col>
                     <Col>
-                      <Form.Group as={Row} controlId="formHorizontalCoilStatus">
+                      <Form.Group as={Row} controlId="formHorizontalOilerMode">
                         <Form.Label column sm={4}>
-                          Coil Satatus
+                          Oiler Mode
                         </Form.Label>
                         <Col sm={8}>
-                          <Form.Control
-                            type="email"
-                            placeholder="Coil Satatus"
-                          />
-                        </Col>
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col>
-                      <Form.Group
-                        as={Row}
-                        controlId="formHorizontalScheduleRemark"
-                      >
-                        <Form.Label column sm={2}>
-                          Schedule Remark
-                        </Form.Label>
-                        <Col sm={10}>
-                          <Form.Control
-                            type="text"
-                            placeholder="Schedule Remark"
-                          />
+                          <Form.Control type="text" placeholder="Oiler Mode" />
                         </Col>
                       </Form.Group>
                     </Col>
@@ -292,29 +307,19 @@ function ModalsInputCoil() {
                     <Col>
                       <Form.Group
                         as={Row}
-                        controlId="formHorizontalScheduledId"
+                        controlId="formHorizontalLowerCoating"
                       >
-                        <Form.Label column sm={4}>
-                          Schedule ID
+                        <Form.Label column sm={3}>
+                          Lower Coating Weight[g/m^2]
                         </Form.Label>
-                        <Col sm={8}>
-                          <Form.Control type="text" placeholder="Schedule ID" />
+                        <Col sm={2}>
+                          <Form.Control type="text" placeholder="Target" />
                         </Col>
-                      </Form.Group>
-                    </Col>
-                    <Col>
-                      <Form.Group
-                        as={Row}
-                        controlId="formHorizontalInputCoilId"
-                      >
-                        <Form.Label column sm={4}>
-                          Input Coil ID
-                        </Form.Label>
-                        <Col sm={8}>
-                          <Form.Control
-                            type="text"
-                            placeholder="Input Coil ID"
-                          />
+                        <Col sm={2}>
+                          <Form.Control type="text" placeholder="Min" />
+                        </Col>
+                        <Col sm={2}>
+                          <Form.Control type="text" placeholder="Max" />
                         </Col>
                       </Form.Group>
                     </Col>
@@ -323,47 +328,19 @@ function ModalsInputCoil() {
                     <Col>
                       <Form.Group
                         as={Row}
-                        controlId="formHorizontalScheduleStatus"
+                        controlId="formHorizontalUpperCoating"
                       >
-                        <Form.Label column sm={4}>
-                          Schedule Status
+                        <Form.Label column sm={3}>
+                          Upper Coating Weight[g/m^2]
                         </Form.Label>
-                        <Col sm={8}>
-                          <Form.Control
-                            type="text"
-                            placeholder="Schedule Status"
-                          />
+                        <Col sm={2}>
+                          <Form.Control type="text" placeholder="Target" />
                         </Col>
-                      </Form.Group>
-                    </Col>
-                    <Col>
-                      <Form.Group as={Row} controlId="formHorizontalCoilStatus">
-                        <Form.Label column sm={4}>
-                          Coil Satatus
-                        </Form.Label>
-                        <Col sm={8}>
-                          <Form.Control
-                            type="email"
-                            placeholder="Coil Satatus"
-                          />
+                        <Col sm={2}>
+                          <Form.Control type="text" placeholder="Min" />
                         </Col>
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col>
-                      <Form.Group
-                        as={Row}
-                        controlId="formHorizontalScheduleRemark"
-                      >
-                        <Form.Label column sm={2}>
-                          Schedule Remark
-                        </Form.Label>
-                        <Col sm={10}>
-                          <Form.Control
-                            type="text"
-                            placeholder="Schedule Remark"
-                          />
+                        <Col sm={2}>
+                          <Form.Control type="text" placeholder="Max" />
                         </Col>
                       </Form.Group>
                     </Col>
@@ -374,7 +351,12 @@ function ModalsInputCoil() {
           </Tabs>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={handleClose}>Close</Button>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSave}>
+            Save Changes
+          </Button>
         </Modal.Footer>
       </Modal>
     </>
